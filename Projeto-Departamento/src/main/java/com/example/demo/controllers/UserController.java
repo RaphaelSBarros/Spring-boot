6b1583105a03323c125624b034c0d589da17e0e5
Raphael Alexsandro <49734953+RaphaelSBarros@users.noreply.github.com>
2023-04-15 01:29:21 -0300

    Add files via upload

---
+++ Projeto-Departamento/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserController {
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping
+	public List<User> findAll(){
+		List<User> result = repository.findAll();
+		return result;
+	}
+	
+}
